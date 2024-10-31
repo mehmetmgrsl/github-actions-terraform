098053dd0858e8e66800e1a12ce54ea8340b3eab
mehmetmgrsl <mehmetgursul@gmail.com>
2024-10-31 16:09:56 +0100

    fixed the actions

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+}
+
+variable "key_name" {
+  description = "Name of the existing EC2 key pair"
+}
+
+variable "name" {
+  description = "Name of EC2 Intance"
+}
+
+variable "ami_name" {
+  description = "name of Amazon Machine Image (AMI)"
+}
