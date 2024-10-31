9915233da1cc03dd5c7fd6fbf843abfb55d1fb1e
mehmetmgrsl <mehmetgursul@gmail.com>
2024-10-31 15:51:18 +0100

    fixed the actions

--- variables.tf
+++ variables.tf
@@ -10,14 +10,10 @@ variable "key_name" {
   description = "Name of the existing EC2 key pair"
 }
 
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for remote state"
-}
-
 variable "name" {
   description = "Name of EC2 Intance"
 }
 
 variable "ami_name" {
-  description = "ID of AMI"
+  description = "name of Amazon Machine Image (AMI)"
 }
