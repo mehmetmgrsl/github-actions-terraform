518a227e9e1cc4b70322cfea2209cbff3a2945b6
mehmetmgrsl <mehmetgursul@gmail.com>
2024-11-08 17:05:28 +0100

    Terraform lint was added

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,14 @@ terraform {
     key            = "terraform.tfstate"
     region         = "eu-central-1"
   }
+  required_version = ">= 1.9" 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
 provider "aws" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,24 @@
 variable "aws_region" {
   description = "AWS region"
+  type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
 }
 
 variable "key_name" {
   description = "Name of the existing EC2 key pair"
+  type        = string
 }
 
 variable "name" {
   description = "Name of EC2 Intance"
+  type        = string
 }
 
 variable "ami_name" {
   description = "name of Amazon Machine Image (AMI)"
+  type        = string
 }
